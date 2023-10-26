3d0b1280f8f296291efae90082f7c4aa1d9af362
tyywrobel <tylerbeitel25@gmail.com>
2023-10-25 20:47:42 -0400

    Add project files.

---
+++ Controllers/AllAboutResultsController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InTheBag.Controllers
+{
+    public class AllAboutResults : Controller
+    {
+        public IActionResult Index()
+        {
+            var weekday = DateTime.Now.DayOfWeek;
+            var day = weekday.ToString();
+            var time = DateTime.Now.Hour;
+
+            // day = "Wednesday";
+
+            if (time <= 6)
+            {
+                ViewBag.Greeting = "It is too early to be up!";
+            }
+            else if (time <= 12)
+            {
+                ViewBag.Greeting = "Good Morning";
+            }
+            else if (time <= 18)
+            {
+                ViewBag.Greeting = "Good Afternoon";
+            }
+            else
+            {
+                ViewBag.Greeting = "Good Evening";
+            }
+            int route = 0;
+
+            switch (day)
+            {
+                case "Monday":
+                case "Tuesday":
+                    ViewData["dayMessage"] = "The work week just started! Stay focused, you have a lot to do this week!";
+                    route = 1;
+                    break;
+                case "Wednesday":
+                    ViewData["dayMessage"] = "Halfway to the weekend!";
+                    route = 2;
+                    break;
+                case "Thursday":
+                    ViewData["dayMessage"] = "Isn't it Friday somewhere?";
+                    route = 3;
+                    break;
+                case "Friday":
+                    ViewData["dayMessage"] = "Woo hoo TGIF";
+                    route = 4;
+                    break;
+                default:
+                    ViewData["dayMessage"] = "Ahhhh the weekend!";
+                    route = 5;
+                    break;
+            }
+            if (route==1)
+            {
+                return RedirectToAction("Weekday","AllAboutResults");
+            }
+            else if (route==2 || route==3)
+            {
+                return Redirect("https://lisabalbach.com/CIT218/Chapter8/HappyWednesday.html");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Weekday()
+        {
+            ViewBag.Greeting = "Congratulations, the work week just started and you have been rerouted!";
+            return View();
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using InTheBag.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace InTheBag.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult IndexViewBag()
+        {
+            IList<string> WishList = new List<string>();
+            WishList.Add("Peace");
+            WishList.Add("Health");
+            WishList.Add("Happiness");
+            ViewBag.WishList = WishList;
+            return View();
+        }
+
+        public IActionResult IndexViewData()
+        {
+            IList<string> WishList = new List<string>();
+            WishList.Add("Quies");
+            WishList.Add("Salutem");
+            WishList.Add("Beatitudinem");
+            ViewData["WishList"] = WishList;
+            return View();
+        }
+
+        public IActionResult IndexTempData()
+        {
+            IList<string> WishList = new List<string>();
+            WishList.Add("La Paz");
+            WishList.Add("La Salud");
+            WishList.Add("La Felicidad");
+            TempData["WishList"] = WishList;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
